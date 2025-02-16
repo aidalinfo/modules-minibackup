51ac6d91d8cae4536579d49c1260c2c4ec4a245b
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-02-16 14:25:05 +0100

    add doc et mysql md

--- modules/official/mysql/cmd/main.go
+++ modules/official/mysql/cmd/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	"modules-minibackup/internal/mysql/pkg/commands"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "module-mysql-mb",
-	Short: "Module MySQL pour MiniBackup",
-	Long: `
-	Ce module permet de gérer les backups et la restauration.
-	`,
-}
-
 func main() {
+	// Crée le rootCmd et y ajoute les commandes backup et restore.
+	rootCmd := &cobra.Command{
+		Use:   "module-mysql-mb",
+		Short: "Module MySQL pour MiniBackup",
+		Long: `
+Ce module permet de gérer les backups et la restauration.
+`,
+	}
 	rootCmd.AddCommand(commands.BackupCmd())
 	rootCmd.AddCommand(commands.RestoreCmd())
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	// Crée le dossier "docs" pour stocker la doc
+	if err := os.MkdirAll("docs", 0755); err != nil {
+		log.Fatalf("Erreur lors de la création du dossier docs: %v", err)
 	}
+
+	// Génère la documentation Markdown pour toutes les commandes
+	if err := doc.GenMarkdownTree(rootCmd, "docs"); err != nil {
+		log.Fatalf("Erreur lors de la génération de la documentation: %v", err)
+	}
+
+	log.Println("Documentation générée dans le dossier docs")
 }
