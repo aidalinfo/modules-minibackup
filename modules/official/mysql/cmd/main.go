7fa2bf5d5277909ff700579889f3f098da4c79bd
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-02-16 18:37:47 +0100

    fix bug module

--- modules/official/mysql/cmd/main.go
+++ modules/official/mysql/cmd/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"modules-minibackup/internal/mysql/pkg/commands"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
-func main() {
-	// Crée le rootCmd et y ajoute les commandes backup et restore.
-	rootCmd := &cobra.Command{
-		Use:   "module-mysql-mb",
-		Short: "Module MySQL pour MiniBackup",
-		Long: `
+
+var rootCmd = &cobra.Command{
+	Use:   "module-mysql-mb",
+	Short: "Module MySQL pour MiniBackup",
+	Long: `
 Ce module permet de gérer les backups et la restauration.
 `,
-	}
+}
+
+func main() {
+
 	rootCmd.AddCommand(commands.BackupCmd())
 	rootCmd.AddCommand(commands.RestoreCmd())
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	// // Crée le dossier "docs" pour stocker la doc
 	// if err := os.MkdirAll("docs", 0755); err != nil {
