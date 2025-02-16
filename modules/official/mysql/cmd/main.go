b5840b40688d959534d10544f887ffa8cf1763a2
Killian-Aidalinfo <killian.stein@aidalinfo.fr>
2025-02-16 16:19:30 +0100

    fix bug doc

--- modules/official/mysql/cmd/main.go
+++ modules/official/mysql/cmd/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"modules-minibackup/internal/mysql/pkg/commands"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
 )
 
 func main() {
@@ -22,15 +18,15 @@ Ce module permet de gérer les backups et la restauration.
 	rootCmd.AddCommand(commands.BackupCmd())
 	rootCmd.AddCommand(commands.RestoreCmd())
 
-	// Crée le dossier "docs" pour stocker la doc
-	if err := os.MkdirAll("docs", 0755); err != nil {
-		log.Fatalf("Erreur lors de la création du dossier docs: %v", err)
-	}
+	// // Crée le dossier "docs" pour stocker la doc
+	// if err := os.MkdirAll("docs", 0755); err != nil {
+	// 	log.Fatalf("Erreur lors de la création du dossier docs: %v", err)
+	// }
 
-	// Génère la documentation Markdown pour toutes les commandes
-	if err := doc.GenMarkdownTree(rootCmd, "docs"); err != nil {
-		log.Fatalf("Erreur lors de la génération de la documentation: %v", err)
-	}
+	// // Génère la documentation Markdown pour toutes les commandes
+	// if err := doc.GenMarkdownTree(rootCmd, "docs"); err != nil {
+	// 	log.Fatalf("Erreur lors de la génération de la documentation: %v", err)
+	// }
 
-	log.Println("Documentation générée dans le dossier docs")
+	// log.Println("Documentation générée dans le dossier docs")
 }
